43d98c67c91426df6a3ac59543e1ac6195c7b8ac
June <junetiger10@naver.com>
2024-09-25 10:05:49 +0900

    first commit

---
+++ src/main/java/com/example/spring20240924/Control/Controller4.java
@@ -0,0 +1,24 @@
+package com.example.spring20240924.Control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main4") //컨트롤러 내의 핸들러 메소드 경로 앞부분
+public class Controller4 {
+
+//request handler method
+    //https://docs.spting.io/spring-framework/reference/web/webmvc/mvc-controller/ann-methods.html
+
+    //localhost:8080/main/sub1
+    @RequestMapping("sub1")
+    public void sub1(){
+        System.out.println("Controller4.sub1");
+    }
+
+    @RequestMapping("sub2")
+    public void sub2(){
+        System.out.println("Controller4.sub2 수정!!!");
+    }
+
+}
