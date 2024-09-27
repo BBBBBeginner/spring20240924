21d44f145fc78a353c3822395b19322a8d7d170e
June <junetiger10@naver.com>
2024-09-27 17:04:28 +0900

    view

--- src/main/java/com/example/spring20240924/Control/Controller06.java
+++ src/main/java/com/example/spring20240924/Control/Controller06.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("main6")
 
-public class Controller6 {
+public class Controller06 {
     @RequestMapping("sub1")
     @ResponseBody
     public String sub1(String search) {
