f0f8e790d8a37c602b4e3685d27d1b37040c6e24
June <junetiger10@naver.com>
2024-10-07 17:22:23 +0900

    javaBeans 사용

---
+++ src/main/java/com/example/spring20240924/dto/c24/Customer.java
@@ -0,0 +1,15 @@
+package com.example.spring20240924.dto.c24;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String name;
+    private String contactName;
+    private String address;
+    private String unit;
+    private String price;
+    private String productid;
+    private String productname;
+
+}
