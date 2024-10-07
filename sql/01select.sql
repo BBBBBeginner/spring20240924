2a953b8b11065fea7bda6d483ceff6a9bc14da4b
June <junetiger10@naver.com>
2024-10-07 17:16:59 +0900

    select

---
+++ sql/01select.sql
@@ -0,0 +1,21 @@
+USE w3schools; # 실행 ctrl enter
+
+SELECT *
+FROM Customers;
+SELECT *
+FROM Employees;
+SELECT *
+FROM Products;
+SELECT *
+FROM Categories;
+
+# Data들은 Table 에 저장됨
+# Table은 행과 열로 구성됨
+
+# 행(row) : record
+# 열(column) : attribute, field
+
+# SELECT : 테이블의 데이터를 조회(가져옴)
+# SELECT * FROM 테이블명;
+SELECT *
+FROM Suppliers;
