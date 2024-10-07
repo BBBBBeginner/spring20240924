b0bbc90e7dfe6689a7826d2a61d425059ebb0573
June <junetiger10@naver.com>
2024-10-07 17:22:29 +0900

    직원 정보

---
+++ src/main/java/com/example/spring20240924/dto/c24/Employees.java
@@ -0,0 +1,10 @@
+package com.example.spring20240924.dto.c24;
+
+import lombok.Data;
+
+@Data
+public class Employees {
+    private String Id;
+    private String FirstName;
+    private String LastName;
+}
