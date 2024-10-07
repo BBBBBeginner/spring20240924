3361829389bd5977db307622e009646f4411d315
June <junetiger10@naver.com>
2024-10-07 17:23:39 +0900

    상품목록 조회

---
+++ src/main/java/com/example/spring20240924/dto/c24/Product.java
@@ -0,0 +1,11 @@
+package com.example.spring20240924.dto.c24;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String Price;
+    private String Name;
+    private String Unit;
+    private String id;
+}
