748840b73b8790671b1d514f78577e85220c77c8
June <junetiger10@naver.com>
2024-10-21 16:49:10 +0900

    마이바티스 메소드에서 javaBean 사용

---
+++ src/main/java/com/example/spring20240924/dto/c38/Customer.java
@@ -0,0 +1,17 @@
+package com.example.spring20240924.dto.c38;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Customer {
+    private Integer id;
+    private String name;
+    private String contactName;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+
+}
