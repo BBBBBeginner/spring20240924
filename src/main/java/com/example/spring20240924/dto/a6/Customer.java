d28e4cfc3ca887cfd61c55d69d505e7d479110b3
June <junetiger10@naver.com>
2024-11-14 10:06:39 +0900

    고객조회, 공급자조회

---
+++ src/main/java/com/example/spring20240924/dto/a6/Customer.java
@@ -0,0 +1,15 @@
+package com.example.spring20240924.dto.a6;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private Integer id;
+    private String name;
+    private String address;
+    private String contact;
+    private String city;
+    private String country;
+    private String postalCode;
+
+}
